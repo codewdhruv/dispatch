c7051869731591e9f11d8db12d4e422f6cc8bd16
Kevin Glisson <kevgliss@gmail.com>
2022-11-14 11:58:56 -0800

    Adding UI

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+from dispatch.plugins.dispatch_slack.decorators import get_organization_scope_from_slug
+
 from dispatch.case.models import Case
 from dispatch.case.messaging import send_case_created_notifications
-from dispatch.database import SessionLocal
 
-db_session = SessionLocal()
-case = db_session.query(Case).one()
+db_session = get_organization_scope_from_slug("default")
+case = db_session.query(Case).first()
 send_case_created_notifications(case, db_session)
