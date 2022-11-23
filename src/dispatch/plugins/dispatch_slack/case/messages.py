2f544f9199cac1f2f8361b67866c5243324a1556
Kevin Glisson <kevgliss@gmail.com>
2022-11-23 15:25:44 -0800

    bolt.py

--- src/dispatch/plugins/dispatch_slack/case/messages.py
+++ src/dispatch/plugins/dispatch_slack/case/messages.py
@@ -1,4 +1,3 @@
-import logging
 from blockkit import (
     Actions,
     Button,
@@ -11,16 +10,12 @@ from blockkit import (
     Modal,
 )
 
-from typing import Optional
-from pydantic import BaseModel
-
-from dispatch.database.core import engine, sessionmaker, SessionLocal
 from dispatch.case import service as case_service
 from dispatch.config import DISPATCH_UI_URL
 from dispatch.enums import DispatchEnum
 from dispatch.case.models import Case
-from dispatch.organization import service as organization_service
-from dispatch.conversation import service as conversation_service
+from dispatch.project import service as project_service
+from dispatch.common.utils.cli import install_plugins
 
 from dispatch.plugins.dispatch_slack.fields import (
     DefaultBlockIds,
@@ -34,12 +29,14 @@ from dispatch.plugins.dispatch_slack.fields import (
     case_type_select,
 )
 
-from dispatch.project import service as project_service
-from dispatch.common.utils.cli import install_plugins
-
-app = AsyncApp()
-
-logging.basicConfig(level=logging.DEBUG)
+from dispatch.plugins.dispatch_slack.bolt import (
+    app,
+    action_context_middleware,
+    db_middleware,
+    button_context_middleware,
+    message_context_middleware,
+    SubjectMetadata,
+)
 
 
 class CaseNotificationActions(DispatchEnum):
@@ -64,84 +61,6 @@ class CaseReportActions(DispatchEnum):
 
 class CaseShortcutCallbacks(DispatchEnum):
     report = "case-report"
-
-
-class SubjectMetadata(BaseModel):
-    id: Optional[str]
-    type: Optional[str]
-    organization_slug: str
-    project_id: Optional[str]
-    channel_id: Optional[str]
-
-
-def button_context_middleware(payload, context, next):
-    """Attempt to determine the current context of the event."""
-    context.update({"subject": SubjectMetadata.parse_raw(payload["value"])})
-    next()
-
-
-def action_context_middleware(body, context, next):
-    """Attempt to determine the current context of the event."""
-    context.update({"subject": SubjectMetadata.parse_raw(body["view"]["private_metadata"])})
-    next()
-
-
-def message_context_middleware(body, context, next):
-    """Attemps to determine the current context of the event."""
-    context.update({"subject": SubjectMetadata(**body["message"]["metadata"]["event_payload"])})
-    next()
-
-
-def slash_command_context_middleware(context, next):
-    db_session = SessionLocal()
-    organization_slugs = [o.slug for o in organization_service.get_all(db_session=db_session)]
-    db_session.close()
-
-    conversation = None
-    for slug in organization_slugs:
-        schema_engine = engine.execution_options(
-            schema_translate_map={
-                None: f"dispatch_organization_{slug}",
-            }
-        )
-
-        scoped_db_session = sessionmaker(bind=schema_engine)()
-        conversation = conversation_service.get_by_channel_id_ignoring_channel_type(
-            db_session=scoped_db_session, channel_id=context["channel_id"]
-        )
-        if conversation:
-            scoped_db_session.close()
-            break
-
-    context.update(
-        {
-            "subject": SubjectMetadata(
-                type="incident",
-                id=conversation.incident.id,
-                organization_slug=conversation.project.organization.slug,
-                project_id=conversation.project.id,
-            )
-        }
-    )
-    next()
-
-
-def db_middleware(context, next):
-    if not context.get("subject"):
-        db_session = SessionLocal()
-        slug = organization_service.get_default(db_session=db_session).slug
-        context.update({"subject": SubjectMetadata(organization_slug=slug)})
-        db_session.close()
-    else:
-        slug = context["subject"].organization_slug
-
-    schema_engine = engine.execution_options(
-        schema_translate_map={
-            None: f"dispatch_organization_{slug}",
-        }
-    )
-    context["db_session"] = sessionmaker(bind=schema_engine)()
-    next()
 
 
 def create_case_notification(case: Case, channel_id: str):
@@ -228,7 +147,7 @@ def create_case_notification(case: Case, channel_id: str):
     return Message(blocks=blocks).build()["blocks"]
 
 
-@app.message("hello")
+# @app.message("hello")
 def message_hello(client, context):
     from dispatch.plugins.dispatch_slack.decorators import get_default_organization_scope
 
